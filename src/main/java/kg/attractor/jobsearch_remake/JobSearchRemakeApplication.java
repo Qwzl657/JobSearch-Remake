451b39b3b5f4323243f1eebd71c10625590079a7
Alibek <sultanovalibek2020@gmail.com>
2026-04-08 17:54:46 +0600

    первый коммит

---
+++ src/main/java/kg/attractor/jobsearch_remake/JobSearchRemakeApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearch_remake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchRemakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/jobsearch_remake/JobSearchRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearch_remake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchRemakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
