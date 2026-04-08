9cbf90d601555839773c3c37289b9a8e65d82985
Alibek <sultanovalibek2020@gmail.com>
2026-04-08 18:34:08 +0600

    Добавил классы в папку model с ссылочным типом данных #2

---
+++ src/main/java/kg/attractor/jobsearch_remake/model/EducationInfo.java
@@ -0,0 +1,24 @@
+package kg.attractor.jobsearch_remake.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EducationInfo {
+
+    private Integer id;
+    private Integer resumeId;
+    private String institution;
+    private String program;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String degree;
+
+}
---
+++ src/main/java/kg/attractor/jobsearch_remake/model/RespondedApplicant.java
@@ -0,0 +1,19 @@
+package kg.attractor.jobsearch_remake.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespondedApplicant {
+
+    private Integer id;
+    private Integer resumeId;
+    private Integer vacancyId;
+    private boolean confirmation;
+
+}
