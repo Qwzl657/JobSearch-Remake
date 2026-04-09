5e99aa97e6d7c62cb40bf75f012abe773f221909
Alibek <sultanovalibek2020@gmail.com>
2026-04-09 10:48:49 +0600

    Изменение UserServiceImpl

--- src/main/java/kg/attractor/jobsearch_remake/service/impl/UserServiceImpl.java
+++ src/main/java/kg/attractor/jobsearch_remake/service/impl/UserServiceImpl.java
@@ -1,19 +1,31 @@
 package kg.attractor.jobsearch_remake.service.impl;
 
+import kg.attractor.jobsearch_remake.dao.UserDao;
 import kg.attractor.jobsearch_remake.dto.UserDto;
+import kg.attractor.jobsearch_remake.model.User;
 import kg.attractor.jobsearch_remake.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserDao userDao;
+
     @Override
-    public UserDto getSampleUser(){
-        UserDto user = new UserDto();
-        user.setUsername("Alex");
-        user.setEmail("qwe@qwe.qwe");
-        user.setPassword("qwerty");
-        return user;
+    public UserDto getSampleUser() {
+        return UserDto.builder()
+                .username("test")
+                .email("test@mail.com")
+                .password("123")
+                .build();
     }
 
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
 }
