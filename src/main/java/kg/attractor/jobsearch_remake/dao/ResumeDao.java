8927e77568a0d9b7cc387f3166164ae57d1c7cc4
Alibek <sultanovalibek2020@gmail.com>
2026-04-09 16:22:21 +0600

    Изменил UserDao,теперь содержит sql запросы

--- src/main/java/kg/attractor/jobsearch_remake/dao/ResumeDao.java
+++ src/main/java/kg/attractor/jobsearch_remake/dao/ResumeDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+
 import java.util.List;
 
 @Component
@@ -14,20 +15,42 @@ public class ResumeDao {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public List<Resume> findAll() {
-        return jdbcTemplate.query("SELECT * FROM resumes",
-                new BeanPropertyRowMapper<>(Resume.class));
+    public List<Resume> findByCategory(Integer categoryId) {
+        String sql = "SELECT * FROM resumes WHERE category_id = ?";
+        return jdbcTemplate.query(sql,
+                new BeanPropertyRowMapper<>(Resume.class), categoryId);
     }
 
-    public List<Resume> findByCategory(String category) {
-        String sql = "SELECT * FROM resumes WHERE category = ?";
-        return jdbcTemplate.query(sql,
-                new BeanPropertyRowMapper<>(Resume.class), category);
+    public void create(Resume r) {
+        String sql = """
+        INSERT INTO resumes (applicant_id, name, category_id, salary, is_active)
+        VALUES (?, ?, ?, ?, ?)
+    """;
+
+        jdbcTemplate.update(sql,
+                r.getApplicantId(),
+                r.getName(),
+                r.getCategoryId(),
+                r.getSalary(),
+                r.isActive());
     }
 
-    public List<Resume> findByUserId(Long userId) {
-        String sql = "SELECT * FROM resumes WHERE user_id = ?";
-        return jdbcTemplate.query(sql,
-                new BeanPropertyRowMapper<>(Resume.class), userId);
+    public void update(Resume r) {
+        String sql = """
+        UPDATE resumes
+        SET name=?, category_id=?, salary=?, is_active=?
+        WHERE id=?
+    """;
+
+        jdbcTemplate.update(sql,
+                r.getName(),
+                r.getCategoryId(),
+                r.getSalary(),
+                r.isActive(),
+                r.getId());
+    }
+
+    public void delete(Integer id) {
+        jdbcTemplate.update("DELETE FROM resumes WHERE id = ?", id);
     }
 }
