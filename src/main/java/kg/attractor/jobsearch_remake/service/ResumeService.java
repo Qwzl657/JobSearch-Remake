e4b3c5877915ac8225290f29922c5f0cf079a8e6
Alibek <sultanovalibek2020@gmail.com>
2026-04-09 16:28:59 +0600

    Изменил RessumeService, перенесли отсюда логику

--- src/main/java/kg/attractor/jobsearch_remake/service/ResumeService.java
+++ src/main/java/kg/attractor/jobsearch_remake/service/ResumeService.java
@@ -1,6 +1,7 @@
 package kg.attractor.jobsearch_remake.service;
 
 import kg.attractor.jobsearch_remake.dao.ResumeDao;
+import kg.attractor.jobsearch_remake.dto.ResumeDto;
 import kg.attractor.jobsearch_remake.model.Resume;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,53 @@ public class ResumeService {
 
     private final ResumeDao resumeDao;
 
-    public List<Resume> getAll() {
-        return resumeDao.findAll();
+    public List<ResumeDto> getAllDto() {
+        return resumeDao.findAll().stream()
+                .map(this::toDto)
+                .toList();
     }
 
-    public List<Resume> getByCategory(String category) {
-        return resumeDao.findByCategory(category);
+    public List<ResumeDto> getByCategory(Integer categoryId) {
+        return resumeDao.findByCategory(categoryId).stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    public void create(ResumeDto dto) {
+        Resume r = Resume.builder()
+                .applicantId(1)
+                .name(dto.getName())
+                .categoryId(dto.getCategoryId())
+                .salary(dto.getSalary())
+                .isActive(dto.isActive())
+                .build();
+
+        resumeDao.create(r);
+    }
+
+    public void update(Integer id, ResumeDto dto) {
+        Resume r = Resume.builder()
+                .id(id)
+                .name(dto.getName())
+                .categoryId(dto.getCategoryId())
+                .salary(dto.getSalary())
+                .isActive(dto.isActive())
+                .build();
+
+        resumeDao.update(r);
+    }
+
+    public void delete(Integer id) {
+        resumeDao.delete(id);
+    }
+
+    private ResumeDto toDto(Resume r) {
+        return ResumeDto.builder()
+                .id(r.getId())
+                .name(r.getName())
+                .categoryId(r.getCategoryId())
+                .salary(r.getSalary())
+                .isActive(r.isActive())
+                .build();
     }
 }
