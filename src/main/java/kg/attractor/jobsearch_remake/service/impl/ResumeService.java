7e171e8023a913f55531f2b9a70c6661e0abb34c
Alibek <sultanovalibek2020@gmail.com>
2026-04-09 10:50:18 +0600

    Добавил ResumeService

---
+++ src/main/java/kg/attractor/jobsearch_remake/service/impl/ResumeService.java
@@ -0,0 +1,23 @@
+package kg.attractor.jobsearch_remake.service.impl;
+
+import kg.attractor.jobsearch_remake.dao.ResumeDao;
+import kg.attractor.jobsearch_remake.model.Resume;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ResumeService {
+
+    private final ResumeDao resumeDao;
+
+    public List<Resume> getAll() {
+        return resumeDao.findAll();
+    }
+
+    public List<Resume> getByCategory(String category) {
+        return resumeDao.findByCategory(category);
+    }
+}
