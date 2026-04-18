aa4228019fe755a6c081bd3f04b8a84d59284418
Alibek <sultanovalibek2020@gmail.com>
2026-04-18 20:51:12 +0600

    Создали новый класс в папке config(SecurityConfig)

---
+++ src/main/java/kg/attractor/jobsearch_remake/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package kg.attractor.jobsearch_remake.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager userDetailsManager() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+
+        manager.setUsersByUsernameQuery(
+                "SELECT email, password, enabled FROM users WHERE email = ?"
+        );
+
+
+        manager.setAuthoritiesByUsernameQuery(
+                "SELECT u.email, r.role FROM users u " +
+                        "JOIN roles r ON u.role_id = r.id " +
+                        "WHERE u.email = ?"
+        );
+
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(httpBasic -> {})
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/vacancies/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/resumes/**").permitAll()
+
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
