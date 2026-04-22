c0ded8721fd3978760b25ffcd72cac5fd1fecd08
Alibek <sultanovalibek2020@gmail.com>
2026-04-22 19:59:58 +0600

    Добавил валидацию в AuthMvcController

--- src/main/java/kg/attractor/jobsearch_remake/controller/mvc/AuthMvcController.java
+++ src/main/java/kg/attractor/jobsearch_remake/controller/mvc/AuthMvcController.java
@@ -1,5 +1,20 @@
 package kg.attractor.jobsearch_remake.controller.mvc;
-
+import jakarta.validation.Valid;
+import kg.attractor.jobsearch_remake.dto.UserDto;
+import kg.attractor.jobsearch_remake.service.ResumeService;
+import kg.attractor.jobsearch_remake.service.UserService;
+import kg.attractor.jobsearch_remake.service.VacancyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
