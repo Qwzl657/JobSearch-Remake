77ebfb39cf6066f4e1442f5f1c6bb0ee5723aa4a
Alibek <sultanovalibek2020@gmail.com>
2026-04-26 14:35:31 +0600

    Обновление моделей(добавление аннотаций) 3#

--- src/main/java/kg/attractor/jobsearch_remake/model/Resume.java
+++ src/main/java/kg/attractor/jobsearch_remake/model/Resume.java
@@ -1,9 +1,7 @@
 package kg.attractor.jobsearch_remake.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -11,15 +9,32 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "resumes")
 public class Resume {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Integer applicantId;
-    private String name;
-    private int categoryId;
-    private double salary;
-    private boolean isActive;
-    private LocalDate createdDate;
-    private LocalDate updateTime;
 
+    @Column(name = "applicant_id")
+    private Integer applicantId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category_id")
+    private int categoryId;
+
+    @Column(name = "salary")
+    private double salary;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "update_time")
+    private LocalDate updateTime;
 }
--- src/main/java/kg/attractor/jobsearch_remake/model/Vacancy.java
+++ src/main/java/kg/attractor/jobsearch_remake/model/Vacancy.java
@@ -1,9 +1,7 @@
 package kg.attractor.jobsearch_remake.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -11,18 +9,41 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "vacancies")
 public class Vacancy {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private String description;
-    private Integer categoryId;
-    private double salary;
-    private Integer expFrom;
-    private Integer expTo;
-    private boolean isActive;
-    private Integer authorId;
-    private LocalDate createdDate;
-    private LocalDate updateTime;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "salary")
+    private double salary;
+
+    @Column(name = "exp_from")
+    private Integer expFrom;
+
+    @Column(name = "exp_to")
+    private Integer expTo;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "author_id")
+    private Integer authorId;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "update_time")
+    private LocalDate updateTime;
 }
