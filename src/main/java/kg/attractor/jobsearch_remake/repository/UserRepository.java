3bffa03a9cf1002ce5a4549067b5c092c3a86bcf
Alibek <sultanovalibek2020@gmail.com>
2026-04-26 14:39:12 +0600

    Создал репозитории под новые модели 1#

---
+++ src/main/java/kg/attractor/jobsearch_remake/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package kg.attractor.jobsearch_remake.repository;
+
+import kg.attractor.jobsearch_remake.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    List<User> findByAccountType(String accountType);
+}
