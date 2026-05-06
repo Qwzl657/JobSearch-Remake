c52cad208024e6ae1aea80a19c9b7967c98d79aa
Alibek <sultanovalibek2020@gmail.com>
2026-05-06 10:46:35 +0600

    В VacancyRepository была добавлена сортировка по количеству откликов через аннотацию Query

--- src/main/java/kg/attractor/jobsearch_remake/repository/VacancyRepository.java
+++ src/main/java/kg/attractor/jobsearch_remake/repository/VacancyRepository.java
@@ -1,18 +1,34 @@
 package kg.attractor.jobsearch_remake.repository;
 
 import kg.attractor.jobsearch_remake.model.Vacancy;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface VacancyRepository extends JpaRepository<Vacancy, Integer> {
+
     List<Vacancy> findByIsActiveTrue();
+
     Page<Vacancy> findByIsActiveTrue(Pageable pageable);
+
     List<Vacancy> findByCategoryId(Integer categoryId);
+
     List<Vacancy> findByAuthorId(Integer authorId);
+
     Page<Vacancy> findByAuthorId(Integer authorId, Pageable pageable);
+
+    //  Сортировка по количеству откликов
+    @Query("""
+            SELECT v FROM Vacancy v
+            LEFT JOIN RespondedApplicant r ON r.vacancyId = v.id
+            WHERE v.isActive = true
+            GROUP BY v.id
+            ORDER BY COUNT(r.id) DESC
+            """)
+    Page<Vacancy> findActiveOrderByResponseCount(Pageable pageable);
 }
