fbcfaeb9543703905af196de9da374c64379cf95
Alibek <sultanovalibek2020@gmail.com>
2026-05-06 10:52:49 +0600

    В ResponseService параметр был переименован на resumeId вместо userId,метод getUserByVacancy получает резюме а после ищет пользователя

--- src/main/java/kg/attractor/jobsearch_remake/service/ResponseService.java
+++ src/main/java/kg/attractor/jobsearch_remake/service/ResponseService.java
@@ -1,5 +1,6 @@
 package kg.attractor.jobsearch_remake.service;
 
+import kg.attractor.jobsearch_remake.dto.ResumeDto;
 import kg.attractor.jobsearch_remake.dto.UserDto;
 import kg.attractor.jobsearch_remake.dto.VacancyDto;
 import kg.attractor.jobsearch_remake.model.RespondedApplicant;
@@ -7,6 +8,7 @@ import kg.attractor.jobsearch_remake.repository.RespondedApplicantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,25 +20,31 @@ public class ResponseService {
     private final RespondedApplicantRepository respondedApplicantRepository;
     private final UserService userService;
     private final VacancyService vacancyService;
+    private final ResumeService resumeService;
 
-    public List<VacancyDto> getVacanciesByUser(Long userId) {
-        log.info("Fetching vacancies for user id: {}", userId);
-        return respondedApplicantRepository.findByResumeId(userId.intValue()).stream()
+    public List<VacancyDto> getVacanciesByResume(Long resumeId) {
+        log.info("Получение вакансий для резюме id: {}", resumeId);
+        return respondedApplicantRepository.findByResumeId(resumeId.intValue()).stream()
                 .map(r -> vacancyService.getById(r.getVacancyId()))
                 .toList();
     }
 
     public List<UserDto> getUsersByVacancy(Long vacancyId) {
-        log.info("Fetching applicants for vacancy id: {}", vacancyId);
+        log.info("Получение соискателей для вакансии id: {}", vacancyId);
         return respondedApplicantRepository.findByVacancyId(vacancyId.intValue()).stream()
-                .map(r -> userService.findById(r.getResumeId()))
+                .map(r -> {
+                    ResumeDto resume = resumeService.getById(r.getResumeId());
+                    return userService.findById(resume.getApplicantId());
+                })
                 .toList();
     }
 
-    public void respond(Long userId, Long vacancyId) {
-        log.info("User id: {} responding to vacancy id: {}", userId, vacancyId);
+
+    @Transactional
+    public void respond(Long resumeId, Long vacancyId) {
+        log.info("Отклик на вакансию id: {} с резюме id: {}", vacancyId, resumeId);
         RespondedApplicant response = RespondedApplicant.builder()
-                .resumeId(userId.intValue())
+                .resumeId(resumeId.intValue())
                 .vacancyId(vacancyId.intValue())
                 .confirmation(false)
                 .build();
