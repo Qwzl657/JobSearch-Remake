f689a83250bd17a81e9304a299e1800e58f98548
Alibek <sultanovalibek2020@gmail.com>
2026-05-07 12:28:35 +0600

    Добавил недостаюшие исключения такие как Category/ContactType/MessageNotFounf

---
+++ src/main/java/kg/attractor/jobsearch_remake/exception/CategoryNotFoundException.java
@@ -0,0 +1,11 @@
+package kg.attractor.jobsearch_remake.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CategoryNotFoundException extends BaseException {
+    public CategoryNotFoundException() {
+        super("Category not found");
+    }
+}
