c929933e587c03ccaa1784ec30ee8d67dc3f403a
Alibek <sultanovalibek2020@gmail.com>
2026-05-17 20:03:15 +0600

    Добавлен REST контроллер для создания резюме через AJAX

---
+++ src/main/java/kg/attractor/jobsearch_remake/controller/ResumeApiController.java
@@ -0,0 +1,33 @@
+package kg.attractor.jobsearch_remake.controller;
+
+import jakarta.validation.Valid;
+import kg.attractor.jobsearch_remake.dto.ResumeDto;
+import kg.attractor.jobsearch_remake.dto.UserDto;
+import kg.attractor.jobsearch_remake.service.ResumeService;
+import kg.attractor.jobsearch_remake.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/resumes")
+@RequiredArgsConstructor
+public class ResumeApiController {
+
+    private final ResumeService resumeService;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody ResumeDto resumeDto,
+                                    Authentication auth) {
+        UserDto user = userService.getByEmail(auth.getName());
+        resumeDto.setApplicantId(user.getId());
+        resumeService.create(resumeDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
